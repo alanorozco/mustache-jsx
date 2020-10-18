6e6f9627513cfb636e0054cb6a8f9d0dadc56828
Alan Orozco <alan@orozco.xyz>
2020-10-18 15:12:46 -0700

    src

---
+++ src/babel-plugin-jsx-excess-fragment.mjs
@@ -0,0 +1,33 @@
+export default function (babel) {
+  const { types: t } = babel;
+
+  return {
+    name: "simpler-jsx",
+    visitor: {
+      CallExpression(path, state) {
+        if (
+          !t.isIdentifier(path.node.callee, {
+            name: state.opts.pragma,
+          })
+        ) {
+          return;
+        }
+        if (
+          !t.isIdentifier(path.node.arguments[0], {
+            name: state.opts.pragmaFrag,
+          })
+        ) {
+          return;
+        }
+        const args = path.node.arguments.slice(2);
+        while (t.isStringLiteral(args[0]) && /^[\s\S\t\n]*$/.test(args[0])) {
+          args.shift();
+        }
+        if (args.length !== 1) {
+          return;
+        }
+        path.replaceWith(args[0]);
+      },
+    },
+  };
+}
