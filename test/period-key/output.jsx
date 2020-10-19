7a59e0fdc854208f54cef00df88a7de8d347b885
Alan Orozco <alan@orozco.xyz>
2020-10-19 12:05:26 -0700

    nested period lookup

--- test/period-key/output.jsx
+++ test/period-key/output.jsx
@@ -6,6 +6,12 @@
       <>
         {" "}
         <p>{view("bar", _0_foo).baz}</p>
+        {section(view("bar", _0_foo), (_1_bar, _i_1) => (
+          <>
+            {" "}
+            <p>{view("x", _1_bar, _0_foo).y.z}</p>
+          </>
+        ))}
       </>
     ))}
   </div>
