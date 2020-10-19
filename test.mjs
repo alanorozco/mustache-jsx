f0bfd59244ed764ffa7d1736a1fe1ce0ee8dc0ba
Alan Orozco <alan@orozco.xyz>
2020-10-19 11:49:18 -0700

    safer test update

--- test.mjs
+++ test.mjs
@@ -1,36 +1,7 @@
-import fs from "fs";
-import Writer from "./src/mustache-jsx.mjs";
 import test from "ava";
-import prettier from "prettier";
+import { cat, convert, all, update } from "./test/runner.mjs";
 
-const base = "test";
-
-const cat = (f) => fs.readFileSync(f).toString();
-
-const convert = (template) =>
-  prettier.format(new Writer().render(template), { parser: "babel" });
-
-function* all() {
-  for (const dir of fs.readdirSync(base)) {
-    const input = `${base}/${dir}/input.mustache`;
-    const expectedJsx = `${base}/${dir}/output.jsx`;
-
-    if (fs.existsSync(input)) {
-      yield [dir, input, expectedJsx];
-    }
-  }
-}
-
-let created = 0;
-for (const [name, input, expectedJsx] of all()) {
-  if (!fs.existsSync(expectedJsx)) {
-    fs.writeFileSync(expectedJsx, convert(cat(input)));
-    console.log("created", expectedJsx);
-    created++;
-  }
-}
-
-console.warn(`  ${created} tests updated`);
+update();
 
 for (const [name, input, expectedJsx] of all()) {
   test(name, async (t) => {
---
+++ test/runner.mjs
@@ -0,0 +1,37 @@
+import fs from "fs";
+import prettier from "prettier";
+import Writer from "../src/mustache-jsx.mjs";
+
+const base = "test";
+
+export const cat = (f) => fs.readFileSync(f).toString();
+
+export const convert = (template) =>
+  prettier.format(new Writer().render(template), { parser: "babel" });
+
+export function* all() {
+  for (const dir of fs.readdirSync(base)) {
+    const input = `${base}/${dir}/input.mustache`;
+    const expectedJsx = `${base}/${dir}/output.jsx`;
+
+    if (fs.existsSync(input)) {
+      yield [dir, input, expectedJsx];
+    }
+  }
+}
+
+export function update(create = false) {
+  let created = 0;
+  for (const [name, input, expectedJsx] of all()) {
+    if (create || !fs.existsSync(expectedJsx)) {
+      fs.writeFileSync(expectedJsx, convert(cat(input)));
+      console.log("created", expectedJsx);
+      created++;
+    }
+  }
+  console.warn(`  ${created} tests updated`);
+}
+
+if (process.argv.includes("--update")) {
+  update(/* create */ true);
+}
