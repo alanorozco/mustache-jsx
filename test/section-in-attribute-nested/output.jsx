bd50a117c2d0a171f3475488c105bdfebebba794
Alan Orozco <alan@orozco.xyz>
2020-10-20 14:02:29 -0700

    add failing test

---
+++ test/section-in-attribute-nested/output.jsx
@@ -0,0 +1,9 @@
+<a
+  class={section(view("classes"), (_0_classes, _i_0) => (
+    <>{section(view("value", _0_classes), (_1_value, _i_1) => _1_value)}$</>
+  ))}
+>
+  TODO: The output for this test is very wrong.
+  <br />
+  class contains fragments, when it should be strings all the way down.
+</a>;
